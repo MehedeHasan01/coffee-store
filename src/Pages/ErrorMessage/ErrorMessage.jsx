a7856069fc7f2b090bd9dd5db76701da05c5e142
MehedeHasan01 <mrrony373383@gmail.com>
2023-12-25 15:30:08 +0600

    Link Add

--- src/Pages/ErrorMessage/ErrorMessage.jsx
+++ src/Pages/ErrorMessage/ErrorMessage.jsx
@@ -1,9 +1,13 @@
 import { Helmet } from "react-helmet";
 import BackToHomeBtn from "../../Components/BackToHomeBtn/BackToHomeBtn";
 import erroeImg from '../../assets/images/404/404.gif'
+import Navbar from "../../Components/Header/Navbar";
+import Footer from "../../Components/Footer/Footer";
 const ErrorMessage = () => {
     return (
-        <div className="max-w-5xl mx-auto px-8 md:px-0 py-12">
+        <div>
+            <Navbar/>
+         <div className="max-w-5xl mx-auto px-8 md:px-0 py-12">
             <Helmet>
                 <title>Coffee Store |Not Found 404</title>
             </Helmet>
@@ -11,6 +15,8 @@ const ErrorMessage = () => {
             <div>
                 <img src={erroeImg} className="h-[70vh] w-full" alt="No Found" />
             </div>
+        </div>
+        <Footer/>
         </div>
     );
 };
